7f9d47800b29ef519aecb8583f3234eaf2939938
Dalia-Ramadan <115746100+Dalia-Ramadan@users.noreply.github.com>
2024-08-28 04:51:16 +0300

    new change

---
+++ bin/problem3.dart
@@ -0,0 +1,15 @@
+/*
+ake a list, say for example this one:
+  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+and write a program that prints out all
+the elements of the list that are less than 5.
+ */
+import 'dart:io';
+void main(){
+  var a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  for(var i in a){
+    if(i<5){
+      print(i);
+    }
+  }
+}
