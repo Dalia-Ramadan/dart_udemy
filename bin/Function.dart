c637f489facef60db470a394d3e46947ca638857
Dalia-Ramadan <115746100+Dalia-Ramadan@users.noreply.github.com>
2024-09-08 04:43:01 +0300

    new change

--- bin/Function.dart
+++ bin/Function.dart
@@ -1,7 +1,30 @@
-void main(){
-print('dart');
-d();
+void main() {
+  func1();
+    //------
+
+  func2('dart');
+  func2(9);
+  func2(!true);
+  func2('*');
+    //------
+  print(func3());
+    //------
+  print(func4(10, 5));
+    //------
+
 }
-d(){
+func1(){
   print('dart');
+}
+//-------------------
+func2(a){
+  print('a = $a');
+}
+//--------------------
+func3(){
+  return 4 * 4;
+}
+//-------------------
+func4(x,y){
+  return x * y;
 }
