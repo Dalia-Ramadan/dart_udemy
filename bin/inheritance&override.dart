ee543f5c2ccfb9b98cd4b928eb7f86060a4bbca9
Dalia-Ramadan <115746100+Dalia-Ramadan@users.noreply.github.com>
2024-09-26 04:01:15 +0300

    new change

---
+++ bin/inheritance&override.dart
@@ -0,0 +1,33 @@
+class A{ // super class // (indirect super class)
+  info(){
+    print('dart');
+  }
+}
+//----------
+class B extends A{ //super class  بالنسبه لل class C  + sub class بالنسبه لل class A // (direct super class)
+  info2(){
+    print('dart2');
+  }
+  @override
+  info(){ // override
+    print('dart3');
+  }
+}
+//---------
+class C extends B{} //sub class لل class B
+//---------
+void main(){
+  A ob = new A();
+  B ob2 = new B();
+  C ob3 = new C();
+  print('-----------------ob');
+  ob.info();
+  print('-----------------ob2');
+  ob2.info();
+  ob2.info2();
+  print('-----------------ob3');
+  ob3.info();
+  ob3.info2();
+  print('-----------------override (ob2)');
+  ob2.info();
+}
